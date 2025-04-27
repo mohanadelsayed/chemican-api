3792d30857439dc00d4e31458760577e90e4ed56
mohanadelsayed <46132362+mohanadelsayed@users.noreply.github.com>
2025-04-27 18:41:19 +0000

    dd

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ app.use(bodyParser.json());
 
 // Database connection
 const db = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQL_PORT
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
 });
 
 // Test connection endpoint
